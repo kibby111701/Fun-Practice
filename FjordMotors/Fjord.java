4238751bfa4dfc41a6cbe979e31f5e0073ad465e
Chris Kraft <kibby111701@gmail.com>
2019-10-23 12:45:00 -0600

    number change

--- FjordMotors/Fjord.java
+++ FjordMotors/Fjord.java
@@ -1,11 +1,11 @@
 public class Fjord{
     public static void main(String[] args) {
 
-        Car takk = new Car("Takk", "Sedan", 50, 8);
-        Car beklager = new Car("Beklager", "Sedan", 40, 7.5);
-        Car vakker = new Car("Vakker", "SUV", 60, 5);
-        Car stygg = new Car("Stygg", "SUV", 55, 6);
-        Car vanskellig = new Car("Vanskellig", "Truck", 65, 4.75);
+        Car takk = new Car("Takk", "Sedan", 40, 9.5);
+        Car beklager = new Car("Beklager", "Sedan", 45, 7.5);
+        Car vakker = new Car("Vakker", "SUV", 60, 7.5);
+        Car stygg = new Car("Stygg", "SUV", 50, 9);
+        Car vanskellig = new Car("Vanskellig", "Truck", 60, 8.75);
         Car lastebill = new Car("Lastebill", "Truck", 70, 5.5);
 
         System.out.println(takk.meetsStandards());
