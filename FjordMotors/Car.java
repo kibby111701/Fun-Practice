5a0a456d78b9240b0e6d06fe8a199586583916d4
Chris Kraft <kibby111701@gmail.com>
2019-10-23 12:07:39 -0600

    finished Fjord motors code

--- FjordMotors/Car.java
+++ FjordMotors/Car.java
@@ -5,6 +5,45 @@ public class Car{
     private double tankSize;
     private double time;
 
+    public Car(String model, String type, double tankSize, double time){
+        this.model = model;
+        this.type = type;
+        this.tankSize = tankSize;
+        this.time = time;
+    }
+
+    public String getModel(){
+        return this.model;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    private double convertTank(double tankSize){
+        return 0.264172 * tankSize;
+
+    }
+
+    public double milesPerGallon(){
+        return (60 * time)/convertTank(tankSize);
+    }
+
+    public String meetsStandards(){
+        if (type.equals("Sedan") && milesPerGallon() >= 50){
+            return model + " meets standards";
+        }
+        else if (type.equals("SUV") && milesPerGallon() >= 40){
+            return model + " meets standards";
+        }
+        else if (type.equals("Truck") && milesPerGallon() >= 30){
+            return model + " meets standards";
+        }
+        else{
+            return model + " does not meet standards";
+        }
+    }
+
 
 
 
--- FjordMotors/Fjord.java
+++ FjordMotors/Fjord.java
@@ -1,5 +1,19 @@
 public class Fjord{
     public static void main(String[] args) {
+
+        Car takk = new Car("Takk", "Sedan", 50, 8);
+        Car beklager = new Car("Beklager", "Sedan", 40, 7.5);
+        Car vakker = new Car("Vakker", "SUV", 60, 5);
+        Car stygg = new Car("Stygg", "SUV", 55, 6);
+        Car vanskellig = new Car("Vanskellig", "Truck", 65, 4.75);
+        Car lastebill = new Car("Lastebill", "Truck", 70, 5.5);
+
+        System.out.println(takk.meetsStandards());
+        System.out.println(beklager.meetsStandards());
+        System.out.println(vakker.meetsStandards());
+        System.out.println(stygg.meetsStandards());
+        System.out.println(vanskellig.meetsStandards());
+        System.out.println(lastebill.meetsStandards());
         
     }
 
